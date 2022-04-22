26dfd2132ccbc91c2f641a114d7f1d3f57c78a5c
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2022-04-22 17:37:34 +0200

    feat: added seed creating new kudos
    Also remove unnecessary notification after create new kudo.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 5.times do |i|
-  Employee.create(email: "emp#{i}@email.com", password: "secret#{i}")
+  Employee.create!(email: "emp#{i}@email.com", password: "secret#{i}")
+end
+
+3.times do |i|
+  Kudo.create!(title: "Seed #{i}", content: "Content #{i}", giver: Employee.find_by!(email: "emp#{i}@email.com"), receiver: Employee.find_by!(email: "emp#{i + 1}@email.com"))
 end
